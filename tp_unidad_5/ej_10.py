82314211ec2fb030d82e78ef0e955226fc0382b6
lucachialvo <lucachialvo@users.noreply.github.com>
2026-04-08 12:46:40 -0300

    cambios en el ej 10, mas legible

--- tp_unidad_5/ej_10.py
+++ tp_unidad_5/ej_10.py
@@ -1,39 +1,36 @@
 # hecho por luca chialvo
 from random import randint
 
-ventasDelDia = []
-ventasProdSemanal = []
+PRODUCTOS = 4
+DIAS = 7
 
-# los datos se generan de forma random; el 1er for es para generar 1 sublista con 7 nros, y el 2do es para repetir esa sublista 4 veces
-ventas = [[randint(0,10)for _ in range(7)] for _ in range(4)] 
+# datos de venta random; 1) for es para generar 1 sublista con 7 nros, y el 2do es para repetir esa sublista 4 veces
+ventas = [[randint(0,10)for _ in range(DIAS)] for _ in range(PRODUCTOS)] 
 n = len(ventas)
 
-#es lo mismo que hacer
-#ventas = [
-#   [randint(0,10)for _ in range(7)], # producto 1
-#   [randint(0,10)for _ in range(7)], # producto 2
-#   [randint(0,10)for _ in range(7)], # producto 3
-#   [randint(0,10)for _ in range(7)] # producto 4
-#]
-
-for dia in range(len(ventas[0])): #sumatoria ventas de los 4 productos durante el dia
-    ventasDelDia.append(sum([ventas[i][dia] for i in range(n)]))
+ventasDelDia = []
+for dia in range(DIAS): #sumatoria ventas de los 4 productos durante el dia
+    ventasDelDia.append(sum(producto[dia] for producto in ventas))
 diaConMasVentas = ventasDelDia.index((max(ventasDelDia)))
 
 
 # TABLA
 print("Prod\tDia 1\tDia 2\tDia 3\tDia 4\tDia 5\tDia 6\tDia 7")
 
-for producto in range(4):
-    print(f"P{producto+1}", end="\t" if producto<4 else "\n")
-    for dia in range(7):
+for producto in range(PRODUCTOS):
+    print(f"P{producto+1}", end="\t")
+    for dia in range(DIAS):
         print(f"{ventas[producto][dia]}", end="\t" if dia<6 else "\n")
 
+
 # DATOS FINALES
+ventasProdSemanal = []
 for i in range(n):
+    totalVentas = sum(ventas[i])
     # calculas las ventas sumando todos los nros dentro de cada producto [[sum de prod1][sum de prod2][sum de prod3][sum de prod 4]]
-    print(f"Ventas P{i+1}: {sum(ventas[i])}") 
-    ventasProdSemanal.append(sum(ventas[i]))
+    print(f"Ventas P{i+1}: {totalVentas}") 
+    ventasProdSemanal.append(totalVentas)
+
 prodMasVendidoSemanal = ventasProdSemanal.index(max(ventasProdSemanal)) + 1
 
 print(f"Dia con mas ventas: {diaConMasVentas+1}")
